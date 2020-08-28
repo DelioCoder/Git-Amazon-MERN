b9070dabedce18259ef75436bfddbcbedae43d9f
PorgrammingGeek02 <davideveriwhere@gmail.com>
2020-08-27 20:03:31 -0500

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const cors = require('cors');
+
+import data from './data';
+
+const app = express();
+
+//middlewares
+
+app.use(cors());
+
+app.use(express.json());
+
+//routes
+
+app.get('/api/products', (req, res) => {
+
+    res.send(data.products);
+
+});
+
+app.get('/api/products/:id', (req, res) => {
+    
+    const productId = req.params.id;
+    const product = data.products.find(x=>x._id === productId);
+    if(product){
+        res.send(product);
+    }
+    else
+    {
+        res.status(404).send({msg: "Product Not Found."})
+    }
+
+});
+
+app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
