8c506a4f9e81515a8845aa2800eefa1947520aac
PorgrammingGeek02 <davideveriwhere@gmail.com>
2020-08-27 20:39:39 -0500

    Second commit && frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import './App.css';
+
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
+function App() {
+
+  const openMenu = () =>{
+
+    document.querySelector(".sidebar").classList.add("open");
+
+  }
+
+  const closeMenu = () =>{
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+              <header className="header">
+                  <div className="brand">
+                      <button onClick={openMenu}>
+                          &#9776;
+                      </button>
+                      <Link to="/">Amazon</Link>
+                  </div>
+                  <div className="header-links">
+                      <a href="cart.html">Cart</a>
+                      <a href="signin">Sign In</a>
+                  </div>
+              </header>
+              <aside className="sidebar">
+                  <h3>Shopping Categories</h3>
+                  <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                  <ul>
+                      <li>
+                          <a href="index.html">Pants</a>
+                      </li>
+                      
+                  </ul>
+                  <ul>
+                      <li>
+                          <a href="index.html">Shirts</a>
+                      </li>
+                  </ul>
+              </aside>
+              <main className="main">
+                  <div className="content">
+                    <Route exact={true} path="/" component={HomeScreen}></Route>
+                    <Route path="/product/:id" component={ProductScreen}></Route>                 
+                    <Route path="/cart/:id?" component={CartScreen}></Route>
+                  </div>      
+              </main>
+              <footer className="footer">
+                  All right reserved.
+              </footer>
+          </div>
+        </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+export default{
+    products: [{
+            _id: '1',
+            name: 'Slim Shirt',
+            category:'Shirts',
+            image:'/images/b1.png',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image:'/images/b1.png',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image:'/images/b1.png',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+    ]
+}
